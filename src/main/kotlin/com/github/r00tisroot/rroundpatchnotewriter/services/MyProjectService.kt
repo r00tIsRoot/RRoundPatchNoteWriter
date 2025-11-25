d8649353557a3950c0cdff65901eb83be8359d2b
GitHub Action <action@github.com>
2025-11-25 00:34:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/r00tisroot/rroundpatchnotewriter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/r00tisroot/rroundpatchnotewriter/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.r00tisroot.rroundpatchnotewriter.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.r00tisroot.rroundpatchnotewriter.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
